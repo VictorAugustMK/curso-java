20ab2be87f27d0e37f91dc6a87d24856e3b65b11
Victor Augusto <victor.macc12@gmail.com>
2025-03-03 18:10:06 -0300

    inheritance_class

---
+++ src/heritage/application/Program.java
@@ -0,0 +1,50 @@
+package heritage.application;
+
+import heritage.entities.Account;
+import heritage.entities.BussinessAccount;
+import heritage.entities.SavingsAccount;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Account acc = new Account(1001, "Alex", 0.0);
+        BussinessAccount bacc = new BussinessAccount(1002, "Maria", 0.0, 500.0);
+
+        // UPCASTING
+        Account acc1 = bacc;
+        Account acc2 = new BussinessAccount(1002, "Bob", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1003, "Anna", 0.0, 0.01);
+
+        // DOWNCASTING
+        BussinessAccount acc4 = (BussinessAccount)acc2;
+        acc4.loan(100.0);
+
+       // BussinessAccount acc5 = (BussinessAccount)acc3;
+
+        if (acc3 instanceof BussinessAccount) {
+            BussinessAccount acc5 = (BussinessAccount)acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan");
+        }
+
+        if (acc3 instanceof SavingsAccount) {
+            SavingsAccount acc5 = (SavingsAccount)acc3;
+            acc5.updateBalence();
+//            System.out.println("Update!");
+        }
+
+        Account acc6 = new Account(1006, "Alex", 1000.0);
+        acc6.withDraw(200);
+        System.out.println(acc6.getBalance());
+
+        Account acc7 = new SavingsAccount(1007, "Maria", 1000.0, 0.01);
+        acc7.withDraw(200);
+        System.out.println(acc7.getBalance());
+
+        Account acc8 = new BussinessAccount(1008, "Bob", 1000.0, 500.0);
+        acc8.withDraw(200);
+        System.out.println(acc8.getBalance());
+    }
+
+}
---
+++ src/heritage/entities/SavingsAccount.java
@@ -0,0 +1,32 @@
+package heritage.entities;
+
+public class SavingsAccount extends Account {
+
+    private Double interrestRate;
+
+    public SavingsAccount() {
+        super();
+    }
+
+    public SavingsAccount(Integer number, String holder, Double balance, Double interrestRate) {
+        super(number, holder, balance);
+        this.interrestRate = interrestRate;
+    }
+
+    public Double getInterrestRate() {
+        return interrestRate;
+    }
+
+    public void setInterrestRate(Double interrestRate) {
+        this.interrestRate = interrestRate;
+    }
+
+    public void updateBalence() {
+        balance += balance * interrestRate;
+    }
+
+    @Override
+    public void withDraw(double amount) {
+        balance -= amount;
+    }
+}
